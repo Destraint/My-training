03a8a0d32cd005b9dfb26266f309f98f8999e2f0
Destraint <139233107+Destraint@users.noreply.github.com>
2024-11-24 18:15:04 +0300

    Update Tk1.py

--- etudes/Tkinter/Tk1.py
+++ etudes/Tkinter/Tk1.py
@@ -1,1 +1,28 @@
+from tkinter import *
 
+root = Tk()
+
+root.title("Калькулятор площади параллелипипеда")
+root.geometry("512x256")
+root["bg"] = "#F0FFFF"
+root.resizable(width=False, height=False)
+
+
+frame = Frame(
+    root,
+    padx = 10,
+    pady = 10
+)
+frame.pack(expand=True)
+frame.pack(fill=Y)
+frame["bg"] = "#F0FFFF"
+
+method_lbl = Label(
+    frame,
+    text="Способ расчёта площади"
+)
+method_lbl["bg"] = "#F0FFFF"
+method_lbl.grid(row=1, column=1)
+
+
+root.mainloop()
